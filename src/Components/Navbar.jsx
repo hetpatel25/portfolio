b1314ee7cfbb8a9edd297d17976f765b954cae31
Het Patel <hetpatel3245@gmail.com>
2024-01-08 16:06:34 +0530

    humburger-menu improvement

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useContext} from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
 
@@ -8,12 +8,17 @@ import menu from '../Assets/menu.svg'
 
 function Navbar() {
   const [settingsVisible, setSettingsVisible] = useState(false);
-
+  const [menuOpenState, setMenuOpenState] = useState(false)
+  
   const showSettings = (event) => {
     event.preventDefault();
     // Handle settings visibility or any other logic here
     setSettingsVisible(!settingsVisible);
   };
+
+  const handleMenu = ()=>{
+     setMenuOpenState(!menuOpenState);
+  }
 
   return (
     <>
@@ -35,18 +40,19 @@ function Navbar() {
 
     {/* For mobile view */}
     <div className='hidden md:block'>
-    <Menu right pageWrapId={ "page-wrap" } customBurgerIcon={ <img src={menu} /> }>
+    <Menu right pageWrapId={ "page-wrap" } customBurgerIcon={ <img src={menu} /> } isOpen={menuOpenState} onOpen={ handleMenu }>
      
-      <Link id="home" className="menu-item  text-[22px] text-cream mb-9 mt-10" to="/">
+      <Link id="home" className="menu-item  text-[22px] text-cream mb-9 mt-10" to="/"  onClick={handleMenu}>
         About
       </Link>
-      <Link id="about" className="menu-item text-[22px] text-cream mb-9 " to="/about">
+      <Link id="about" className="menu-item text-[22px] text-cream mb-9 " to="/skills"  onClick={handleMenu}>
         Skills
       </Link>
-      <Link id="contact" className="menu-item text-[22px] text-cream mb-9 " to="/contact">
+      <Link id="contact" className="menu-item text-[22px] text-cream mb-9 " to="/projects"  onClick={handleMenu}>
         Projects
       </Link>
-      <Link className="menu-item text-[22px] text-cream mb-9" to="/contact ">Resume</Link>
+      <Link className="menu-item text-[22px] text-cream mb-9" to="/ " >Resume</Link>
+      <Link className="menu-item text-[22px] text-cream mb-9" to="/contact "  onClick={handleMenu}>Contact</Link>
 
        {/* <a
         onClick={showSettings}
